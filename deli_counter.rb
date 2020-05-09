6ef57eec0edc44943cbca55f3e4cff4905de4598
Raveen K Sharma <raveen08@gmail.com>
2020-05-09 03:31:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+katz_deli = []
+
+
+def line(katz_deli)
+  
+  line_status = "The line is currently:"
+  
+  if katz_deli == []
+    puts "The line is currently empty."
+  
+  else
+    katz_deli.each_with_index do |person, number|
+      line_status << " #{number+1}. #{person}"
+      
+  
+    end
+    puts line_status
+
+  end  
+  
+
+end
+
+def take_a_number (katz_deli, line)
+  
+  puts "Welcome, #{katz_deli}, you are number #{line} in line."
+  end
+end
+
